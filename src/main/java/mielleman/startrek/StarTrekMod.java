b8f145f720d39f6888d41d86ccc172cae2df2235
Emiel <emiel.slootman.201@gmail.com>
2015-03-22 12:02:11 +0100

    And again...

--- src/main/java/mielleman/startrek/StarTrekMod.java
+++ src/main/java/mielleman/startrek/StarTrekMod.java
@@ -1,5 +1,9 @@
 package mielleman.startrek;
 
+import mielleman.startrek.reference.References;
+import cpw.mods.fml.common.Mod;
+
+@Mod(modid = References.MODID, version = References.VERSION)
 public class StarTrekMod {
 
 }
--- src/main/java/mielleman/startrek/reference/References.java
+++ src/main/java/mielleman/startrek/reference/References.java
@@ -1,5 +1,8 @@
 package mielleman.startrek.reference;
 
 public class References {
+	
+	public static final String MODID = "StarTrekMod";
+	public static final String VERSION = "1.0.0";
 
 }
