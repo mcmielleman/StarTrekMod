6f70741df979570987c331928fdf07ae052a3834
Emiel <emiel.slootman.201@gmail.com>
2015-03-22 12:05:26 +0100

    Added init event

--- src/main/java/mielleman/startrek/StarTrekMod.java
+++ src/main/java/mielleman/startrek/StarTrekMod.java
@@ -2,8 +2,17 @@ package mielleman.startrek;
 
 import mielleman.startrek.reference.References;
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
 
 @Mod(modid = References.MODID, version = References.VERSION)
 public class StarTrekMod {
-
+	
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+    	
+    }
 }
+
+
