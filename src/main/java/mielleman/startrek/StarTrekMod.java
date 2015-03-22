48510b04dcf5b82ae8b912f9e16704bf9ef2bc87
Emiel <emiel.slootman.201@gmail.com>
2015-03-22 12:13:06 +0100

    Added PreInit and PostInit

--- src/main/java/mielleman/startrek/StarTrekMod.java
+++ src/main/java/mielleman/startrek/StarTrekMod.java
@@ -2,17 +2,28 @@ package mielleman.startrek;
 
 import mielleman.startrek.reference.References;
 import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = References.MODID, name = References.NAME, version = References.VERSION)
 public class StarTrekMod {
 	
-    @EventHandler
-    public void init(FMLInitializationEvent event)
-    {
-    	
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+
     }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+
+    }
+
 }
 
 
