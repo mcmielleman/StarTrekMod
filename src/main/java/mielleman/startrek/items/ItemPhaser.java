34f3389e1aee7963d74123dafe9be20cd91bd54c
Emiel <emiel.slootman.201@gmail.com>
2015-03-24 19:39:23 +0100

    Theoreticle fixing icon
    Doesnt work though

--- src/main/java/mielleman/startrek/items/ItemPhaser.java
+++ src/main/java/mielleman/startrek/items/ItemPhaser.java
@@ -1,19 +1,28 @@
 package mielleman.startrek.items;
 
+import javax.swing.Icon;
+
 import mielleman.startrek.entities.EntityLaser;
 import mielleman.startrek.reference.References;
+import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.IIcon;
 import net.minecraft.world.World;
 import cpw.mods.fml.common.registry.GameRegistry;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 
 public class ItemPhaser extends Item {
 
+	@SideOnly(Side.CLIENT)
+	private IIcon icon;
+	
 	public ItemPhaser() {
         setUnlocalizedName(References.MODID + "_" + ModItems.phaserItemName); 
-        setTextureName(References.MODID + ":" + "p");
+        //setTextureName(References.MODID + ":" + "p");
         GameRegistry.registerItem(this, ModItems.phaserItemName);
         this.maxStackSize = 1;
 	}
@@ -32,6 +41,18 @@ public class ItemPhaser extends Item {
     	}
     	return itemstack;
     }
+    
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void registerIcons(IIconRegister register){
+    	icon = register.registerIcon(References.MODID + ":" + ModItems.phaserItem);
+    }
+    
+	@Override
+	@SideOnly(Side.CLIENT)
+	public IIcon getIconFromDamage(int dmg) {
+		return icon;
+	}
 	
  
 
